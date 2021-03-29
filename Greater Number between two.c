b040da1c71b90342b107d16659aaa20dd7484f7c
Harsh Bawaskar <80563542+harsh-bawaskar@users.noreply.github.com>
2021-03-29 12:41:56 +0530

    Add files via upload

---
+++ Greater Number between two.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n1;
+	int n2;
+
+	printf("\n Enter Num1");
+	scanf("%d",&n1);
+	printf("\n Enter Num2");
+	scanf("%d",&n2);
+	
+	if(n1>n2)
+	{printf("Number 1 is bigger");
+	}
+	else
+	{printf("Number 2 is bigger");
+	}
+	
+}
+	
